05eaffdad8b4614c1cfe9d3d966370aae3245c3d
bonggyoson <bonggyoson@gmail.com>
2023-08-24 16:07:05 +0900

    feat : 박스 view 컨트롤러 추가

---
+++ src/main/java/wods/crossfit/box/controller/view/BoxViewController.java
@@ -0,0 +1,18 @@
+package wods.crossfit.box.controller.view;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class BoxViewController {
+
+    @GetMapping("/box")
+    public String box() {
+        log.info("box controller");
+        return "box/box";
+    }
+}
