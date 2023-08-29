407f5a0af0f2832058734ee3261ef5ca6f672940
bonggyoson <bonggyoson@gmail.com>
2023-08-29 14:03:30 +0900

    feat : db 정보 분기 처리 및 .gitignore 추가

--- src/main/java/wods/crossfit/CrossfitApplication.java
+++ src/main/java/wods/crossfit/CrossfitApplication.java
@@ -2,11 +2,14 @@ package wods.crossfit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
+@PropertySource("classpath:/application-db.yml")
 @SpringBootApplication
 public class CrossfitApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(CrossfitApplication.class, args);
     }
