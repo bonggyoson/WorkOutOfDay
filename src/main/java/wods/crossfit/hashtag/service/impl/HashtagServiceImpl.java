d7129c0a3bcbeb5c20ddb16da3a3ec96da09fb53
bonggyoson <bonggyoson@gmail.com>
2023-10-10 22:45:26 +0900

    fix : 해시태그 기능 수정 중

--- src/main/java/wods/crossfit/hashtag/service/impl/HashtagServiceImpl.java
+++ src/main/java/wods/crossfit/hashtag/service/impl/HashtagServiceImpl.java
@@ -1,20 +1,13 @@
 package wods.crossfit.hashtag.service.impl;
 
-import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.webjars.NotFoundException;
 import wods.crossfit.hashtag.domain.Hashtag;
-import wods.crossfit.hashtag.domain.dto.HashtagDto;
+import wods.crossfit.hashtag.domain.dto.HashtagDto.HashtagRequest;
 import wods.crossfit.hashtag.repository.HashtagRepository;
 import wods.crossfit.hashtag.service.HashtagService;
-import wods.crossfit.workout.domain.Workout;
-import wods.crossfit.workout.repository.WorkoutRepository;
-import wods.crossfit.workoutHashtag.domain.WorkoutHashtag;
-import wods.crossfit.workoutHashtag.domain.dto.WorkoutHashtagDto;
-import wods.crossfit.workoutHashtag.repository.WorkoutHashtagRepository;
 
 @Service
 @RequiredArgsConstructor
@@ -23,30 +16,19 @@ public class HashtagServiceImpl implements HashtagService {
 
     private final HashtagRepository hashtagRepository;
 
-    private final WorkoutRepository workoutRepository;
-
-    private final WorkoutHashtagRepository workoutHashtagRepository;
-
     @Override
     @Transactional
-    public void saveHashtag(List<HashtagDto.HashtagRequest> hashtags, long id) {
-        Workout workout = workoutRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException("해당 오늘의 운동이 존재하지 않습니다."));
+    public List<Hashtag> saveHashtag(List<HashtagRequest> hashtags) {
 
-        List<Hashtag> tags = new ArrayList<>();
-        for (HashtagDto.HashtagRequest hashtag : hashtags) {
-            tags.add(new HashtagDto.HashtagRequest().toEntity(hashtag.getContent()));
-        }
+        return hashtagRepository.saveAll(
+                new HashtagRequest().toEntity(hashtags));
+    }
 
-        List<Hashtag> resultHashtag = hashtagRepository.saveAll(tags);
-        List<WorkoutHashtag> workoutHashtags = new ArrayList<>();
-        for (Hashtag hashtag : resultHashtag) {
-            workoutHashtags.add(
-                    new WorkoutHashtagDto.WorkoutHashtagRequest().toEntity(workout, hashtag));
-        }
+    @Override
+    public List<Hashtag> updateHashtag(long workoutId, List<HashtagRequest> hashtags) {
 
-        workoutHashtagRepository.saveAll(workoutHashtags);
-
+        return hashtagRepository.saveAll(
+                new HashtagRequest().toEntity(hashtags));
     }
 
     @Override
