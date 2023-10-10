df923519166f8fc7df8c37da39862dbdcd0d93c8
bonggyoson <bonggyoson@gmail.com>
2023-10-10 22:45:07 +0900

    fix : 홈 컨트롤러 수정

--- src/main/java/wods/crossfit/home/controller/view/HomeViewController.java
+++ src/main/java/wods/crossfit/home/controller/view/HomeViewController.java
@@ -3,15 +3,21 @@ package wods.crossfit.home.controller.view;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import wods.crossfit.workout.service.WorkoutService;
 
 @Controller
 @Slf4j
 @RequiredArgsConstructor
 public class HomeViewController {
 
+    private final WorkoutService workoutService;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("workout", workoutService.findHotWorkout());
+
         return "home";
     }
 }
